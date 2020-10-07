1d133dbe8a75c5640e4007d733a93d0b76c3fe95
David Furey <david@furey.me.uk>
2020-10-07 18:12:29 +0100

    Separate caching

--- src/CachedYoutubeClient.ts
+++ src/CachedYoutubeClient.ts
@@ -1,32 +1,59 @@
 import { YoutubeClientImpl } from "./youtube";
 import { LiveBroadcast, LiveStream, YoutubeClient } from "./YoutubeClient";
 
-export class CachedYoutubeClient extends YoutubeClient {
-  underlying = new YoutubeClientImpl
 
-  cachedLiveStreams: LiveStream[] | null = null
-  cachedLiveBroadcasts: LiveBroadcast[] | null = null
+interface CachedItem<T> {
+  value: T;
+  expiry: Date;
+}
 
+class Cache {
   cacheDisabledUntil: Date = new Date(0)
 
-  private cacheEnabled(): boolean {
+  private enabled(): boolean {
     return this.cacheDisabledUntil < new Date()
   }
 
-  private disableCache(): void {
-    this.cacheDisabledUntil = new Date(new Date().getTime() + 30000)
-    this.cachedLiveBroadcasts = null
-    this.cachedLiveStreams = null
+  disableForTimeout(): void {
+    console.log("Disabling cache")
+    this.cacheDisabledUntil = new Date(new Date().getTime() + 60000)
   }
+
+  getOrSet<T>(key: string, fn: () => Promise<T>): Promise<T> {
+    const cached = this.youtubeCache[key]
+    console.log(cached)
+    if (cached && this.enabled() && cached.expiry > new Date()) {
+      console.log(`Returning cached value for ${key}`)
+      return Promise.resolve(cached.value as T)
+    } else {
+      console.log("Using uncached version")
+      const result = fn()
+      result.then((v) => { this.youtubeCache[key] = {
+        value: v,
+        expiry: new Date(new Date().getTime() + 300000)
+      }
+      console.log(this.youtubeCache)
+      })
+      return result
+    }
+  }
+
+  private youtubeCache: Record<string, CachedItem<unknown>> = {
+  }
+}
+
+export class CachedYoutubeClient extends YoutubeClient {
+  underlying = new YoutubeClientImpl
+  cache = new Cache()
 
   createLiveBroadcast(
     title: string,
     description: string,
-    thumbnail: { mimeType: "image/jpeg" | "image/png"; data: Buffer; },
+    thumbnail: { mimeType: "image/jpeg" | "image/png"; data: Buffer },
     scheduledStartTime: Date,
     streamId: string,
     privacyStatus?: "public" | "private"): Promise<string> {
-    this.disableCache()
+    this.cache.disableForTimeout()
     return this.underlying.createLiveBroadcast(
       title,
       description,
@@ -42,26 +69,17 @@ export class CachedYoutubeClient extends YoutubeClient {
     description: string,
     scheduledStartTime: Date
   ): Promise<string> {
-    this.disableCache()
+    this.cache.disableForTimeout()
     return this.underlying.updateLiveBroadcast(id, title, description, scheduledStartTime)
   }
 
   updateBroadcastStatus(id: string, status: "live" | "testing" | "complete"): Promise<string> {
-    this.disableCache()
+    this.cache.disableForTimeout()
     return this.underlying.updateBroadcastStatus(id, status)
   }
 
   liveBroadcasts: () => Promise<LiveBroadcast[]> = () => {
-    if (this.cachedLiveBroadcasts) {
-      console.log("Returning cached live broadcasts")
-      return Promise.resolve(this.cachedLiveBroadcasts)
-    } else {
-      const result = this.underlying.liveBroadcasts()
-      if (this.cacheEnabled()) {
-        result.then((broadcasts) => { this.cachedLiveBroadcasts = broadcasts })
-      }
-      return result
-    }
+    return this.cache.getOrSet('liveBroadcasts', this.underlying.liveBroadcasts)
   }
 
   liveStream(id: string): Promise<LiveStream[]> {
@@ -69,16 +87,7 @@ export class CachedYoutubeClient extends YoutubeClient {
   }
 
   liveStreams: () => Promise<LiveStream[]> = () => {
-    if (this.cachedLiveStreams) {
-      console.log("Returning cached live streams")
-      return Promise.resolve(this.cachedLiveStreams)
-    } else {
-      const result = this.underlying.liveStreams()
-      if (this.cacheEnabled()) {
-        result.then((streams) => { this.cachedLiveStreams = streams })
-      }
-      return result
-    }
+    return this.cache.getOrSet('liveStreams', this.underlying.liveStreams)
   }
 
   liveBroadcast(id: string): Promise<LiveBroadcast | null> {
