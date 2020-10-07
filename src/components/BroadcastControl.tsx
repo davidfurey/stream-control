6d2d16058cdc838de9214dc9edb3b5910adf2828
David Furey <david@furey.me.uk>
2020-10-07 18:13:20 +0100

    Allow prepare without confirming that stream status is active
    We don't have enough quota to poll the stream status

--- src/components/BroadcastControl.tsx
+++ src/components/BroadcastControl.tsx
@@ -26,20 +26,19 @@ export class BroadcastControl extends Component<BroadcastControlProps, {}> {
   state = {}
 
   render(): JSX.Element {
-    this.props.stream.status
-
-    if (this.props.stream.status === "active" || this.props.broadcast.status === "live") {
-      switch (this.props.broadcast.status) {
-        case "ready": return <button className="btn btn-primary" onClick={(): void => setStatus(this.props.broadcast.id, "testing")}>Prepare</button>
-        case "live": return <button className="btn btn-warning" onClick={(): void => setStatus(this.props.broadcast.id, "complete")}>Stop</button>
-        case "liveStarting": return <button className="btn btn-danger" disabled>Starting</button>
-        case "testing": return <button className="btn btn-danger" onClick={(): void => setStatus(this.props.broadcast.id, "live")}>Start</button>
-        case "testStarting": return <button className="btn btn-secondary" disabled>Test starting</button>
-        case "complete": return <button className="btn btn-secondary" disabled>Complete</button>
-        default: return <button className="btn btn-secondary" disabled>Broadcast not ready</button>
-      }
-    } else {
-      return <button className="btn btn-secondary" disabled>Stream not ready</button>
+    switch (this.props.broadcast.status) {
+      case "ready": return <button className="btn btn-primary" onClick={(): void => setStatus(this.props.broadcast.id, "testing")}>Prepare</button>
+      case "live": return <button className="btn btn-warning" onClick={(): void => setStatus(this.props.broadcast.id, "complete")}>Stop</button>
+      case "liveStarting": return <button className="btn btn-danger" disabled>Starting</button>
+      case "testing":
+        if (this.props.stream.status === "active") {
+          return <button className="btn btn-danger" onClick={(): void => setStatus(this.props.broadcast.id, "live")}>Start</button>
+        } else {
+          return <button className="btn btn-secondary" disabled>Stream not active</button>
+        }
+      case "testStarting": return <button className="btn btn-secondary" disabled>Test starting</button>
+      case "complete": return <button className="btn btn-secondary" disabled>Complete</button>
+      default: return <button className="btn btn-secondary" disabled>Broadcast not ready</button>
     }
   }
 }
