14e97ac7cd07e4d32110aad4b682bd0782c9a615
David Furey <david@furey.me.uk>
2020-10-12 09:11:40 +0100

    Add implementation of PA monitoring

--- src/pa_status.ts
+++ src/pa_status.ts
@@ -1,3 +1,15 @@
+interface PaStatus {
+  pa: "on" | "off";
+}
+
 export function paState(): Promise<"on" | "off"> {
-  return Promise.resolve("off")
+  return fetch('http://stream-control:5000/status').then((response) => {
+    if (response.status >= 200 || response.status < 300) {
+      response.json().then((body) => {
+        const parsed: PaStatus = body
+        return parsed.pa
+      })
+    }
+    throw new Error('Failed to fetch pa status');
+  })
 }
