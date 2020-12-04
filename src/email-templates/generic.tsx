50d48fd4229ffb3423c5dbe49fd7677e41f364b8
David Furey <david@furey.me.uk>
2020-12-04 22:34:17 +0000

    Check in missing email templates

---
+++ src/email-templates/generic.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { EventRunner } from "../eventrunner";
+import { RunningEvent } from "../events";
+
+export function applicationStart(): JSX.Element {
+  return <>
+    <h1>Stream automation</h1>
+    <p>Application starting</p>
+  </>
+}
+
+export function scheduledTaskMissed(jobName: string, interval: string, lastRan: Date): JSX.Element {
+  return <>
+    <h1>Scheduled task missed</h1>
+    <p>Should run task {jobName} every {interval}, but last check was at {lastRan}`</p>
+  </>
+}
+
+export function startingEvent(name: string, event: RunningEvent): JSX.Element {
+  return <>
+    <h1>Event starting</h1>
+    <p>Preparing to stream event {name},
+    which is due to broadcast at {event.scheduledStartTime}</p>
+    <h2>Running order</h2>
+    <table>
+      <tr>
+        <th>Time</th>
+        <th>Action</th>
+        <th>Parameter</th>
+      </tr>
+      { event.steps.map((step, i) =>
+        <tr key={i}>
+          <td>{
+            step.referenceTime === "Relative" ? "+" + step.offset.toISOString() :
+            new Date(event.scheduledStartTime.getTime() + step.offset.asMilliseconds())
+          }</td>
+          <td>{step.action}</td>
+          <td>{step.parameter1}</td>
+        </tr>
+      )}
+    </table>
+  </>
+}
+
+export function stepFailure(
+  name: string,
+  event: RunningEvent,
+  stepId: number,
+  error: string
+): JSX.Element {
+  return <>
+    <h1>Event error</h1>
+    <p>An error occured while streaming {name}</p>
+    <pre>{error}</pre>
+    <h2>Running order</h2>
+    <table>
+      <tr>
+        <th>Scheduled Time</th>
+        <th>Start Time</th>
+        <th>End Time</th>
+        <th>Action</th>
+        <th>Parameter</th>
+        <th>Result</th>
+      </tr>
+      { event.steps.map((step, i) =>
+        <tr key={i} style={{ backgroundColor: i === (stepId - 1) ? "red" : "default"}}>
+          <td>{
+            step.referenceTime === "Relative" ? "+" + step.offset.toISOString() :
+            new Date(event.scheduledStartTime.getTime() + step.offset.asMilliseconds())
+          }</td>
+          <td>{step.startTime}</td>
+          <td>{step.endTime}</td>
+          <td>{step.action}</td>
+          <td>{step.parameter1}</td>
+          <td>{step.message}</td>
+        </tr>
+      )}
+    </table>
+  </>
+}
+
+export function overrunEvent(runner: EventRunner): JSX.Element {
+  return <>
+    <h1>Event overrun</h1>
+    <p>{runner.name} has overrun.  It was due to finish by {runner.lastEventTime}
+    but is was still running at {Date.now()}. Youtube event has been stopped but
+    cameras may still be on.</p>
+    <h2>Running order</h2>
+    <table>
+      <tr>
+        <th>Scheduled Time</th>
+        <th>Start Time</th>
+        <th>End Time</th>
+        <th>Action</th>
+        <th>Parameter</th>
+        <th>Result</th>
+      </tr>
+      { runner.event.steps.map((step, i) =>
+        <tr key={i}>
+          <td>{
+            step.referenceTime === "Relative" ? "+" + step.offset.toISOString() :
+            new Date(runner.event.scheduledStartTime.getTime() + step.offset.asMilliseconds())
+          }</td>
+          <td>{step.startTime}</td>
+          <td>{step.endTime}</td>
+          <td>{step.action}</td>
+          <td>{step.parameter1}</td>
+          <td>{step.message}</td>
+        </tr>
+      )}
+    </table>
+  </>
+}
