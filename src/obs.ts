7ebc694b1a16814ee8779d101211e8ceb13df75a
David Furey <david@furey.me.uk>
2020-12-19 09:27:27 +0000

    Type guard for OBSError

--- src/obs.ts
+++ src/obs.ts
@@ -1,8 +1,17 @@
-import OBSWebSocket from 'obs-websocket-js';
+import OBSWebSocket, { ObsError } from 'obs-websocket-js';
 
 const obs = new OBSWebSocket();
 
 //obs.connect({ address: 'localhost:4444' });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isObsError(err: any): err is ObsError {
+  return typeof err === 'object' &&
+    err !== null &&
+    err.status === "error" &&
+    typeof err.messageId === 'string' &&
+    typeof err.error === 'string'
+}
 
 function connected(): Promise<void> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
